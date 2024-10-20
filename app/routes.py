adad0f41c716ad64d7754536277680936f125c9f
Fabiano <37879989+rafaelfabiano@users.noreply.github.com>
2024-10-19 22:24:41 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,17 @@
-from flask import Blueprint, render_template, Flask
+from flask import Blueprint, current_app, render_template, Flask
 
-app = Flask(__name__)
 
 user_routes = Blueprint('user_routes', __name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+@user_routes.route('/')
 def users():
     return render_template('inicio.html')
 
 @user_routes.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
-
-# Registrar o Blueprint
-app.register_blueprint(user_routes)
 
 if __name__ == "__main__":
     app.run()
