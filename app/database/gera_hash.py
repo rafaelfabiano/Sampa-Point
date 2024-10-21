b7118764c7ed2894a47408a4050745ff83e80964
Fabiano <37879989+rafaelfabiano@users.noreply.github.com>
2024-10-21 09:46:00 -0300

    Delete app/database directory

--- app/database/gera_hash.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-# Gerando o hash da senha
-senha_plana = "minhasenha123"
-password_hash = bcrypt.hashpw(senha_plana.encode('utf-8'), bcrypt.gensalt())
-
-print(password_hash.decode('utf-8'))
